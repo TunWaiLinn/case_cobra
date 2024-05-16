d14d3bff0608d96d0c878615abf9cad98450cb23
TunWaiLinn <tunwailinn007@gmail.com>
2024-05-16 16:12:21 +0630

    Initial commit

---
+++ src/components/HandleCornerComponent.tsx
@@ -0,0 +1,10 @@
+const HandleCornerComponent = () => {
+  return (
+    <div
+      className="w-5 h-5 rounded-full shadow border bg-white border-zinc-200
+  transition hover:bg-primary"
+    />
+  );
+};
+
+export default HandleCornerComponent;
